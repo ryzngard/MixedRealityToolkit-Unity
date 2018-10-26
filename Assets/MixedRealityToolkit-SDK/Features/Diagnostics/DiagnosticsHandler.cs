8650b22c348049112c770ceff221d54e4790c065
Andrew Hall <ryzngard@live.com>
2018-10-26 11:51:32 -0700

    Add GPU timing dll and hook into diagnostics

--- Assets/MixedRealityToolkit-SDK/Features/Diagnostics/DiagnosticsHandler.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Diagnostics/DiagnosticsHandler.cs
@@ -3,6 +3,9 @@
 
 using Microsoft.MixedReality.Toolkit.Core.EventDatum.Diagnostics;
 using Microsoft.MixedReality.Toolkit.Core.Interfaces.Diagnostics;
+using Microsoft.MixedReality.Toolkit.Core.Services;
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
+using Microsoft.MixedReality.Toolkit.SDK.DiagnosticsSystem.GpuTiming;
 using System;
 using System.Text;
 using UnityEngine;
@@ -27,6 +30,47 @@ namespace Microsoft.MixedReality.Toolkit.SDK.DiagnosticsSystem
                     if (!showCpu)
                     {
                         cpuUseTracker.Reset();
+                    }
+                }
+            }
+        }
+
+        private bool showGpuTiming;
+        private bool ShowGpuTiming
+        {
+            get { return showGpuTiming; }
+            set
+            {
+                if (showGpuTiming != value)
+                {
+                    showGpuTiming = value;
+
+                    var camera = CameraCache.Main;
+
+                    if (camera == null)
+                    {
+                        if (showGpuTiming)
+                        {
+                            Debug.Log("GPU timing cannot be enabled while camera is null");
+                        }
+                        return;
+                    }
+
+                    var gpuTimingCamera = camera.gameObject.GetComponent<GpuTimingCamera>();
+                    if (showGpuTiming)
+                    {
+                        if (gpuTimingCamera == null)
+                        {
+                            camera.gameObject.AddComponent<GpuTimingCamera>();
+                        }
+                        else
+                        {
+                            gpuTimingCamera.enabled = true;
+                        }
+                    }
+                    else if (gpuTimingCamera != null)
+                    {
+                        gpuTimingCamera.enabled = false;
                     }
                 }
             }
@@ -68,6 +112,7 @@ namespace Microsoft.MixedReality.Toolkit.SDK.DiagnosticsSystem
             this.ShowMemory = eventData.ShowMemory;
             this.ShowFps = eventData.ShowFps;
             this.enabled = eventData.Visible;
+            this.ShowGpuTiming = this.enabled;
         }
 
         private void UpdateIsShowingInformation()
@@ -105,6 +150,12 @@ namespace Microsoft.MixedReality.Toolkit.SDK.DiagnosticsSystem
             {
                 var reading = memoryUseTracker.GetReading();
                 displayText.AppendLine($"Memory: {Math.Round(BytesToMB(reading.GCMemoryInBytes), 2)} MB");
+            }
+
+            if (ShowGpuTiming)
+            {
+                var reading = GpuTiming.GpuTiming.GetTime("Frame");
+                displayText.AppendLine($"GPU :  {Math.Round(reading, 2)}");
             }
         }
 
